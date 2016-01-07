f01e78688ae36c80d981a1d8d36e941fe64dec96
bjornnes <steinerikbjornnes@gmail.com>
2016-01-07 11:12:26 +0100

    hgfghuj

---
+++ src/main/java/mapper/FagMapper.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapper;
+
+
+import beans.Fag;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
+/**
+ *
+ * @author Stein-Erik
+ */
+public class FagMapper implements RowMapper<Fag>{
+
+    @Override
+    public Fag mapRow(ResultSet rs, int i) throws SQLException {
+        Fag fag = new Fag();
+        fag.setFagID(rs.getString("id"));
+        
+    }
+    
+}
