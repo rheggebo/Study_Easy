0bcd00fb4cd8512deaa7b989615f29ef81fd5f36
sindregjerde <sindregj@student.hist.no>
2016-01-11 16:57:09 +0100

    fikser tes

--- src/test/java/testing/beans/BrukerBTest.java
+++ src/test/java/testing/beans/BrukerBTest.java
@@ -7,45 +7,87 @@ package testing.beans;
 
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+//import static org.mockito.Mockito.*;
 import java.util.ArrayList;
-
 import beans.BrukerB;
 import beans.KalenderEvent;
+//import org.mockito.Mockito;
 /**
  *
  * @author Sindre
  */
 public class BrukerBTest {
+    BrukerB test;
+    BrukerB objektTest = new BrukerB();
 
 
 //public class SearchEngineJUnitTest {
     //@InjectMocks private PageReader reader;
-    KalenderEvent[] kalenderEvent; 
+    //KalenderEvent[] kalenderEvent; 
     
     @BeforeClass
     public static void setUpClass() throws Exception {
-        // Koden her eksekveres før første test i klassen
+        // Koden her eksekveres fï¿½r fï¿½rste test i klassen
     }
 
     @Before
     public void setUp() throws Exception {
-        // Koden her eksekveres før hver av testmetodene i klassen
-        kalenderEvent=mock(KalenderEvent[].class);
+        // Koden her eksekveres fï¿½r hver av testmetodene i klassen
+        test.setNavn("Per");
+        test.setEpost("per@hotmail.no");
+        test.setNotat("Jeg liker fotball");
+        test.setTilgangsniva(3);
+        // Koden her eksekveres fÃ¸r hver av testmetodene i klassen
+        //kalenderEvent=mock(KalenderEvent[].class);
     
     }
 
     @Test
     public void test_1 (){
+        assertEquals(test.getNavn(),"Per");
+        assertEquals(test.getEpost(),"per@hotmail.no");
+        assertEquals(test.getNotat(),"Jeg liker fotball");
+        assertEquals(test.getTilgangsniva(),3);
+    }
+    
+    /*
+    
+    @Test   
+    public void testInnlogget(){
+        test.setInnlogget(true);
+        assertTrue(test.isInnlogget());
+    }
+    
+    @Test
+    public void testIkkeInnlogget(){
+        test.setInnlogget(false);
+        assertFalse(test.isInnlogget());
+        
+    }
+        
+    /**@Test
+    public void objektTesting(){
+        assertSame(objektTest.getKlasse(), objektTest.getKlasse());
+        
+        //assertNotSame(objektTest.getKlasse(), objektTest.getKlasse());
+                
+    } **/
+        /** EKSEMPEL PÃ… MOCKITO-TESTING
+         * 
+         * //Create mock
+        BrukerB test = Mockito.mock(BrukerB.class);
+        
+        when(test.getNavn()).thenReturn("Ingvild");
+        
+        assertEquals(test.getNavn(),"Ingvild");
+        **/
+        
+        
         /**
          Tester enkle get-metoder som:
          getId(), getType(), getEier(), getRom()
          **/
-        
-        
 
-    }
-    
     @Test
     public void test_2 (){
         /**
@@ -62,19 +104,19 @@ public class BrukerBTest {
     }
     /*
     @Test(expected = NullPointerException.class)
-    public void test_at_koden_som kjøres_kaster_en_NullPointerException() {
-        // Kode som gjør noe annet
+    public void test_at_koden_som kjï¿½res_kaster_en_NullPointerException() {
+        // Kode som gjï¿½r noe annet
     }
     */
 
     @After
     public void tearDown() throws Exception {
-        // Koden her kjøres etter hver test
+        // Koden her kjï¿½res etter hver test
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
-        // Koden her kjører etter alle testmetodene i klassen er ferdig
+        // Koden her kjï¿½rer etter alle testmetodene i klassen er ferdig
     }
 }
 
