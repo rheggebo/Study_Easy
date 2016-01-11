7dc28c83e644c37c15145e3cb4736041addfdf6f
sindregjerde <sindregj@student.hist.no>
2016-01-11 12:33:38 +0100

    La til Junit og mockito i dependencies

--- src/main/java/testing/beans/BrukerBTest.java
+++ src/main/java/testing/beans/BrukerBTest.java
@@ -4,11 +4,59 @@
  * and open the template in the editor.
  */
 package testing.beans;
-
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 /**
  *
  * @author Sindre
  */
 public class BrukerBTest {
-    
+
+
+public class SearchEngineJUnitTest {
+    //@InjectMocks private PageReader reader;
+
+    @BeforeClass
+    public void setUpClass() throws Exception {
+        // Koden her eksekveres før første test i klassen
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // Koden her eksekveres før hver av testmetodene i klassen
+
+    }
+
+    @Test
+    public void test_har_indeksert (){
+
+    }
+
+
+    public void test_riktig_rekkefølge (){
+        
+    }
+
+    public void test_ukjent_org (){
+
+    }
+    /*
+    @Test(expected = NullPointerException.class)
+    public void test_at_koden_som kjøres_kaster_en_NullPointerException() {
+        // Kode som gjør noe annet
+    }
+    */
+
+    @After
+    public void tearDown() throws Exception {
+        // Koden her kjøres etter hver test
+    }
+
+    @AfterClass
+    public void tearDownClass() throws Exception {
+        // Koden her kjører etter alle testmetodene i klassen er ferdig
+    }
+}
+
 }
