937cbaf12ab5b1ffe2ab291e1e13437f43d1e26a
Ane <aneehss@gmail.com>
2016-01-12 15:28:20 +0100

    Merge origin/master
    Conflicts:
    	src/test/java/testing/beans/RomTest.java

---
+++ src/test/java/testing/kontroller/SokeKontrollerTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing.kontroller;
+
+import kontroller.SokeKontroller;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ane
+ */
+public class SokeKontrollerTest {
+    
+    public SokeKontrollerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
