bcb69c98853d85c8312e15c72ceb1b5b20fe80eb
Kjasparov <kasperrostvold@158.38.209.255>
2016-01-18 13:46:20 +0100

    søkefunksjon

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -87,7 +87,11 @@ public interface Service {
     
     public List<KalenderEvent> getAlleEventsFraBruker(BrukerB b);
     
-    public List<Rom> finnRomTypeStorrelse(Rom r);
+    public List<Rom> getRomTypeStorrelse(Rom r);
+    
+    public List<Rom> getRom(Rom r, KalenderEvent ke, boolean storrelse, boolean sitteplasser);
+    
+    public Rom getRom(Rom r);
     
     public List<RomBestilling> getAlleBestillingerFraBruker(BrukerB b);
     
