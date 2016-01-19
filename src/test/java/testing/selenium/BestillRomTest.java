1f031e78c8d8a8236c06ca02b813a4469d7eee8d
ingvild <ingvlgr@student.hist.no>
2016-01-19 15:08:11 +0100

    Revert "Commit :poop:"
    This reverts commit 9b7a79cb0d1360bb39c05d85b7c269ed9df7893b.

---
+++ src/test/java/testing/selenium/BestillRomTest.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing.selenium;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author Ingvild
+ */
+public class BestillRomTest {
+     WebDriver driver = new FirefoxDriver();     
+    
+    
+    @Test
+    public void testBestillRom() throws Throwable {   
+        driver.get("http://localhost:8084/Study_Easy/");
+        
+        Thread.sleep(2000);
+        driver.findElement(By.id("epostInnlogging")).sendKeys("test1@aol.com");
+        
+        Thread.sleep(2000);
+        driver.findElement(By.id("passordInnlogging")).sendKeys("passord"); 
+        
+        Thread.sleep(2000);
+        driver.findElement(By.id("loggInnKnapp")).click(); 
+        
+        Thread.sleep(2000);
+        driver.findElement(By.id("romValg")).click(); 
+   
+        Thread.sleep(2000);
+        driver.findElement(By.id("bestillRom")).click();
+        Thread.sleep(2000);
+
+    } 
+    
+    public BestillRomTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
